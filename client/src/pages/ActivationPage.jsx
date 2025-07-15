f55ada09dc2eebbcb847894c38ae7724b7ebe287
Waqar <waqar216447@gmail.com>
2025-07-15 13:01:27 +0500

    Fixs

--- client/src/pages/ActivationPage.jsx
+++ client/src/pages/ActivationPage.jsx
@@ -2,8 +2,6 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-require('dotenv').config();
-
 const ActivationPage = () => {
   const { activation_token } = useParams();
   const [status, setStatus] = useState("loading");
